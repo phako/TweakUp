561cf119289ae5e6a4b987d0e7658235c3289cd1
Jens Georg <mail@jensge.org>
2012-03-18 18:32:44 +0100

    Add french translation

---
+++ i18n/tr_fr.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.0">
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Synchronize settings</source>
+        <translation>Synchroniser les paramètres</translation>
+    </message>
+    <message>
+        <source>Configuration</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <source>Audio transcoding</source>
+        <translation>Transcodage audio</translation>
+    </message>
+    <message>
+        <source>Share media files that do not have a DLNA profile</source>
+        <translation>Partager des fichiers multimédias qui n&apos;ont pas un profil DLNA</translation>
+    </message>
+    <message>
+        <source>Other UPnP devices may upload media files to the phone</source>
+        <translation>D&apos;autres périphériques UPnP peuvent envoyer des fichiers multimédias sur le téléphone</translation>
+    </message>
+    <message>
+        <source>UPnP devices may remove files uploaded via UPnP</source>
+        <translation>Des périphériques UPnP peuvent supprimer des fichiers transférés via UPnP</translation>
+    </message>
+    <message>
+        <source>Name on network</source>
+        <translation>Nom sur le réseau</translation>
+    </message>
+    <message>
+        <source>Only share when connected to this access point</source>
+        <translation>Ne partager que lorsque connecté à ce point d&apos;accès</translation>
+    </message>
+    <message>
+        <source>Tweak media sharing settings</source>
+        <translation>Personnaliser les paramètres de partage de médias</translation>
+    </message>
+</context>
+<context>
+    <name>NotAvailable</name>
+    <message>
+        <source>Network media sharing is not available in this version. Please update to PR1.2 if available</source>
+        <translation>Le partage de médias par réseau n&apos;est pas disponible dans cette version. Veuillez mettre à jour vers la version PR1.2 si disponible</translation>
+    </message>
+</context>
+</TS>
